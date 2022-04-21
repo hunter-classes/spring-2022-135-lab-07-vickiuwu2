e9d26a3191d62b2c78ebd95f95aef33b80ae8323
vickiuwu2 <vickiuwu2@DESKTOP-OO68MJM.localdomain>
2022-04-20 20:39:04 -0400

    change made for partner to fix -Vicki Wu

--- indent.cpp
+++ indent.cpp
@@ -3,15 +3,3 @@
 
 #include "indent.h"
 
-int countChar(std::string line, char c)
-{
-    int count = 0;
-    for (int i = 0; i < line.length(); i++)
-    {
-        if (line[i] == c)
-        {
-            count++;
-        }
-    }
-    return count;
-}
